03fd821ca019f2ecca7502f3d7cb997c1d8b1787
tchappel <thomas.chappel4@gmail.com>
2024-02-20 18:26:10 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
